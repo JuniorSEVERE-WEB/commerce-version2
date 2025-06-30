bc0fcbd15d79e59dcabb2155b6dca9ace3aa0114
Junior SEVERE <severejunior2017@gmail.com>
2025-06-30 00:25:53 -0400

    Listing Page1

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,7 +1,15 @@
 from django import forms 
 from .models import Listing
+from django.core.validators import URLValidator
 
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
         fields = ["title", "description", "starting_bid", "image_url", "category"]
+
+
+        def clean_image_url(self):
+            url = self.cleaned_data.get('image_url')
+            if url and not url.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                raise forms.ValidationError("Please enter a valid image URL (PNG, JPG, GIF)")
+            return url
--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,38 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import ListingForm
-from .models import User
+from .models import User, Listing
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_listings = Listing.objects.filter(is_active=True)
+    return render(request, "auctions/index.html", {
+        "listings": active_listings
+    })
+
+def listing_detail(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    
+    # Vérifie si l'user connecté a cette annonce dans sa watchlist
+    on_watchlist = False
+    if request.user.is_authenticated:
+        on_watchlist = listing.watchers.filter(pk=request.user.pk).exists()
+    
+    return render(request, "auctions/listing_detail.html", {
+        "listing": listing,
+        "on_watchlist": on_watchlist
+    })
+def toggle_watchlist(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    
+    if request.user.is_authenticated:
+        if listing.watchers.filter(pk=request.user.pk).exists():
+            listing.watchers.remove(request.user)  # Retire de la watchlist
+        else:
+            listing.watchers.add(request.user)  # Ajoute à la watchlist
+    
+    return HttpResponseRedirect(reverse("listing_detail", args=(listing_id,)))
 
 
 def create_listing(request):
